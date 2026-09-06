6c0010c8b5790ea454a12bc35fced02a35fe5505
Rle-Golden <arlecarreno@gmail.com>
2026-09-06 15:21:15 -0500

    a

--- cliente-1/src/App.js
+++ cliente-1/src/App.js
@@ -1,15 +1,22 @@
-import  NavBarAdministrador  from './Componentes/NavBarAdministrador';
+import './App.css';
+import { useState } from 'react';
+import NavBarAdministrador from 'C:/Users/ARLEY/OneDrive/Documentos/.github/Java-grupo1/cliente-1/src/Componentes/NavBarAdministrador.js';
+import AdministradorIndex from  './Views/Administrador/Index.Js';
 
-function App() {
+function app {
+
   return (
     <div>
       <header>
-        
+
         <NavBarAdministrador />
+
+        <a href='./Administradir/Index.JSP'>Ingresar</a>
 
       </header>
     </div>
   );
+  
 }
 
 export default App;
